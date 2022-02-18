d6d4744f70ca0c53b3c9e2a78a269d9e259d9463
Hemant Kaushik <thehemantkaushik@gmail.com>
2022-02-18 12:12:50 +0530

    add detailScreen.dart

---
+++ lib/screens/detailScreen/detailScreen.dart
@@ -0,0 +1,13 @@
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
